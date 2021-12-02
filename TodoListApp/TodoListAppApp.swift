9408653448eac9848f28a7e2dd48915bb0855bbd
German Hernandez <german220291@gmail.com>
2021-12-02 11:20:48 +0000

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by German Hernandez on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
